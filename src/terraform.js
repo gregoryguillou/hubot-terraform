4ff69528655bff2fe163bf2499cd5532a53f5435
gregoryguillou <gregory.guillou@missena.com>
2018-04-01 19:03:55 +0200

    Sample interaction

--- src/terraform.js
+++ src/terraform.js
@@ -1,7 +1,23 @@
 'use strict'
 
+// Description
+//   A hubot script that interact with the Terraform API
+//
+// Configuration:
+//   LIST_OF_ENV_VARS_TO_SET
+//
+// Commands:
+//   hubot hello - <what the respond trigger does>
+//   orly - <what the hear trigger does>
+//
+// Notes:
+//   <optional notes required for the script>
+//
+// Author:
+//   Gregory Guillou <gregory.guillou@resetlogs.com>
+
 module.exports = (robot) => {
-  robot.respond(/terraform\shelp/i, (message) => {
-    message.reply('Welcome to Hubot scripts for Terraform. It is not working yet!')
+  robot.respond(/terraform\shelp/, (message) => {
+    message.reply('Hello! Terraform API scripts are not implemented yet, be patient...')
   })
 }
